2515ba058439e2c5f36aaee9053869ec2c66279c
Adam Musciano <amusciano@gmail.com>
2019-11-06 20:03:06 -0500

    added test_dfi_list

--- tests/test_dfi_list.py
+++ tests/test_dfi_list.py
@@ -1,37 +1,26 @@
-test_output="""
-  "data": [
-    {
-      "analysis_completed": true,
-      "classification": "UNKNOWN",
-      "file_type": "DOC",
-      "first_seen": "Wed, 06 Nov 2019 20:20:28 GMT",
-      "inquest_alerts": [],
-      "last_inquest_featext": "Wed, 06 Nov 2019 20:22:30 GMT",
-      "len_code": 0,
-      "len_context": 21,
-      "len_metadata": 1020,
-      "len_ocr": 0,
-      "mime_type": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-      "sha256": "6d741cc4414b78327f8f27fc5ae35e2f99dd730646d122655dae72bb00e4ae5a",
-      "size": 13236,
-      "subcategory": "maldoc_hunter",
-      "subcategory_url": "https://github.com/InQuest/yara-rules/blob/master/labs.inquest.net/maldoc_hunter.rule"
-    },
-    {
-      "analysis_completed": true,
-      "classification": "MALICIOUS",
-      "file_type": "OLE",
-      "first_seen": "Wed, 06 Nov 2019 20:20:27 GMT",
-      "inquest_alerts": [],
-      "last_inquest_featext": "Wed, 06 Nov 2019 20:23:42 GMT",
-      "len_code": 0,
-      "len_context": 0,
-      "len_metadata": 668,
-      "len_ocr": 0,
-      "mime_type": "application/cdfv2",
-      "sha256": "1b0e70bb03836c27cafadfd4e12a5ea42c4cc65d61ebcbd18cfad02b10a4ced1",
-      "size": 2233344,
-      "subcategory": "maldoc_hunter",
-      "subcategory_url": "https://github.com/InQuest/yara-rules/blob/master/labs.inquest.net/maldoc_hunter.rule"
-    }]
-"""
+import pytest
+import sys, os
+import json
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+from inquestlabs import inquestlabs_api
+from inquestlabs import inquestlabs_exception
+
+@pytest.fixture
+def labs():
+    labs = inquestlabs_api()
+    return labs
+
+@pytest.fixture
+def labs_with_key():
+    labs_api = inquestlabs_api(api_key="mock")
+    return labs_api
+
+
+def test_dfi_list(labs):
+    dfi_list= labs.dfi_list()
+    assert len(dfi_list) == 1337
+
+def test_dfi_list_with_key(labs_with_key):
+    dfi_list= labs_with_key.dfi_list()
+    assert len(dfi_list) == 1337
