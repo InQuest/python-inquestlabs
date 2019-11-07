1a35818338d0a4a5f100e08be156495f9357462d
Adam Musciano <amusciano@gmail.com>
2019-11-06 20:05:08 -0500

    added test_stats

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,96 +1,32 @@
-sample_output="""
-{
-  "data": {
-    "dfidb": {
-      "first_record": "2017-11-15",
-      "macro_hunter": 492068,
-      "maldoc_hunter": 117064,
-      "phish_hunter": 962,
-      "rtf_hunter": 8343,
-      "swfdoc_hunter": 1249
-    },
-    "dfiiocs": {
-      "domain": 5069373,
-      "email": 1629742,
-      "filename": 8717180,
-      "first_record": "2017-11-15",
-      "ip": 1856731,
-      "url": 4075522,
-      "xmpid": 985015
-    },
-    "iocdb": {
-      "domain": 24406,
-      "first_record": "2019-03-22",
-      "hash": 22360,
-      "ipaddress": 21445,
-      "url": 38497,
-      "yarasignature": 8507
-    },
-    "mime": {
-      "application/cdfv2": 131745,
-      "application/msword": 151781,
-      "application/octet-stream": 3872,
-      "application/vnd.ms-excel": 160919,
-      "application/vnd.ms-office": 5907,
-      "application/vnd.ms-outlook": 9012,
-      "application/vnd.openxmlformats-officedocument.presentationml.presentation": 5140,
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": 98997,
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document": 40768,
-      "application/zip": 5032,
-      "first_record": "2017-11-15"
-    },
-    "mime_high": {
-      "DOC": 198456,
-      "DOCX": 5032,
-      "EML": 9012,
-      "OLE": 131745,
-      "OTHER": 3872,
-      "PPT": 5140,
-      "XLS": 259916,
-      "first_record": "2017-11-15"
-    },
-    "repdb": {
-      "asn_num": 490,
-      "domain": 395391,
-      "first_record": "2018-04-01",
-      "ip": 5285453,
-      "url": 1299479
-    },
-    "repdbsources": {
-      "abuse.ch": 556,
-      "alienvault": 1084460,
-      "bambenek": 12197,
-      "binarydefence": 123845,
-      "blocklist": 1912745,
-      "botscout": 69577,
-      "bruteforceblocker": 19056,
-      "ciarmy": 887942,
-      "cleantalk": 93800,
-      "csirtg": 86620,
-      "cybercrime-tracker": 2177,
-      "dataplane": 450459,
-      "emd": 255406,
-      "fedotracker": 3930,
-      "first_record": "2017-07-18",
-      "greensnow": 210190,
-      "isc.sans": 20903,
-      "malcode": 1051,
-      "malwaredomainlist": 3263,
-      "myip": 110129,
-      "openphish": 439857,
-      "packetmail": 447,
-      "phishtank": 165546,
-      "spamhaus": 490,
-      "sslbl": 604,
-      "stopforumspam": 15608,
-      "talos": 23227,
-      "threatweb": 745488,
-      "urlhaus": 234989,
-      "vxvault": 6053,
-      "zeus": 198
-    }
-  },
-  "success": true
-}
-"response=200"
-"""
+import pytest
+import sys, os
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+from inquestlabs import inquestlabs_api
+from inquestlabs import inquestlabs_exception
+
+@pytest.fixture
+def labs():
+    labs = inquestlabs_api()
+    return labs
+
+@pytest.fixture
+def labs_with_key():
+    labs_api = inquestlabs_api(api_key="mock")
+    return labs_api
+
+@pytest.fixture
+def mock_stats():
+    return {'dfidb': {'first_record': '2017-11-15', 'macro_hunter': 492225, 'maldoc_hunter': 117095, 'phish_hunter': 962, 'rtf_hunter': 8347, 'swfdoc_hunter': 1249}, 'dfiiocs': {'domain': 5074406, 'email': 1634109, 'filename': 8721261, 'first_record': '2017-11-15', 'ip': 1856861, 'url': 4077783, 'xmpid': 985797}, 'iocdb': {'domain': 24426, 'first_record': '2019-03-22', 'hash': 22370, 'ipaddress': 21457, 'url': 38528, 'yarasignature': 8508}, 'mime': {'application/cdfv2': 131773, 'application/msword': 151869, 'application/octet-stream': 3872, 'application/vnd.ms-excel': 160930, 'application/vnd.ms-office': 5907, 'application/vnd.ms-outlook': 9017, 'application/vnd.openxmlformats-officedocument.presentationml.presentation': 5144, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 99013, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 40795, 'application/zip': 5032, 'first_record': '2017-11-15'}, 'mime_high': {'DOC': 198571, 'DOCX': 5032, 'EML': 9017, 'OLE': 131773, 'OTHER': 3872, 'PPT': 5144, 'XLS': 259943, 'first_record': '2017-11-15'}, 'repdb': {'asn_num': 490, 'domain': 395391, 'first_record': '2018-04-01', 'ip': 5286304, 'url': 1299516}, 'repdbsources': {'abuse.ch': 556, 'alienvault': 1084460, 'bambenek': 12198, 'binarydefence': 123845, 'blocklist': 1913435, 'botscout': 69602, 'bruteforceblocker': 19056, 'ciarmy': 887942, 'cleantalk': 93800, 'csirtg': 86620, 'cybercrime-tracker': 2177, 'dataplane': 450594, 'emd': 255406, 'fedotracker': 3930, 'first_record': '2017-07-18', 'greensnow': 210190, 'isc.sans': 20903, 'malcode': 1051, 'malwaredomainlist': 3263, 'myip': 110129, 'openphish': 439857, 'packetmail': 447, 'phishtank': 165546, 'spamhaus': 490, 'sslbl': 604, 'stopforumspam': 15608, 'talos': 23227, 'threatweb': 745488, 'urlhaus': 235026, 'vxvault': 6053, 'zeus': 198}}
+
+def test_stats(labs,mock_stats,mocker):
+    mocker.patch('inquestlabs.inquestlabs_api.API' ,return_value=mock_stats)
+
+    stats = labs.stats()
+    assert "dfidb" in stats.keys()
+
+def test_stats_with_key(labs_with_key,mock_stats,mocker):
+    mocker.patch('inquestlabs.inquestlabs_api.API' ,return_value=mock_stats)
+    stats= labs_with_key.stats()
+    assert "dfidb" in stats.keys()
