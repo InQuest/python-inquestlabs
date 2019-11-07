e02cee0b123fff009c28aa2cf5ce0b3cee21cd65
Adam Musciano <amusciano@gmail.com>
2019-11-07 16:22:34 -0500

    added tests for yara_b64re

--- tests/test_yara_b64re.py
+++ tests/test_yara_b64re.py
@@ -1,19 +1,56 @@
-test_output="""
-{
-  "data": "([\\x2b\\x2f-9A-Za-z][3HXn]BlZHJhb[Q-Za-f][159BFJNRVZdhlptx]hbWlua[Q-Za-f]|[\\x2b\\x2f-9A-Za-z]{2}[159BFJNRVZdhlptx]wZWRyYW[0-3][\\x2b\\x2f-9A-Za-z]YW1pbm[k-n]|cGVkcmFt([\\x2b\\x2f-9A-Za-z][\\x2b\\x2f-9A-Za-z]|[\\x2b\\x2f-9A-Za-z])*[\\x2b\\x2f-9A-Za-z][159BFJNRVZdhlptx]hbWlua[Q-Za-f]|cGVkcmFtYW1pbm[k-n]|[\\x2b\\x2f-9A-Za-z][3HXn]BlZHJhbWFtaW5p|[\\x2b\\x2f-9A-Za-z]{2}[159BFJNRVZdhlptx]wZWRyYW1hbWlua[Q-Za-f]|cGVkcmFt([\\x2b\\x2f-9A-Za-z][\\x2b\\x2f-9A-Za-z]|[\\x2b\\x2f-9A-Za-z])*[\\x2b\\x2f-9A-Za-z][2GWm]FtaW5p|[\\x2b\\x2f-9A-Za-z][3HXn]BlZHJhb([\\x2b\\x2f-9A-Za-z][\\x2b\\x2f-9A-Za-z]|[\\x2b\\x2f-9A-Za-z])*[\\x2b\\x2f-9A-Za-z][159BFJNRVZdhlptx]hbWlua[Q-Za-f]|[\\x2b\\x2f-9A-Za-z]{2}[159BFJNRVZdhlptx]wZWRyYW([\\x2b\\x2f-9A-Za-z][\\x2b\\x2f-9A-Za-z]|[\\x2b\\x2f-9A-Za-z])*[\\x2b\\x2f-9A-Za-z]{2}[2GWm]FtaW5p|[\\x2b\\x2f-9A-Za-z][3HXn]BlZHJhb([\\x2b\\x2f-9A-Za-z][\\x2b\\x2f-9A-Za-z]|[\\x2b\\x2f-9A-Za-z])*[\\x2b\\x2f-9A-Za-z]{2}[2GWm]FtaW5p|[\\x2b\\x2f-9A-Za-z][3HXn]BlZHJhb([\\x2b\\x2f-9A-Za-z][\\x2b\\x2f-9A-Za-z]|[\\x2b\\x2f-9A-Za-z])*[\\x2b\\x2f-9A-Za-z]{2}YW1pbm[k-n]|cGVkcmFt([\\x2b\\x2f-9A-Za-z][\\x2b\\x2f-9A-Za-z]|[\\x2b\\x2f-9A-Za-z])*[\\x2b\\x2f-9A-Za-z]{2}[2GWm]FtaW5p|cGVkcmFt([\\x2b\\x2f-9A-Za-z][\\x2b\\x2f-9A-Za-z]|[\\x2b\\x2f-9A-Za-z])*[\\x2b\\x2f-9A-Za-z]{2}YW1pbm[k-n]|cGVkcmFt[\\x2b\\x2f-9A-Za-z][2GWm]FtaW5p|[\\x2b\\x2f-9A-Za-z]{2}[159BFJNRVZdhlptx]wZWRyYW([\\x2b\\x2f-9A-Za-z][\\x2b\\x2f-9A-Za-z]|[\\x2b\\x2f-9A-Za-z])*[\\x2b\\x2f-9A-Za-z]{2}YW1pbm[k-n]|[\\x2b\\x2f-9A-Za-z][3HXn]BlZHJhb([\\x2b\\x2f-9A-Za-z][\\x2b\\x2f-9A-Za-z]|[\\x2b\\x2f-9A-Za-z])*[\\x2b\\x2f-9A-Za-z][2GWm]FtaW5p|[\\x2b\\x2f-9A-Za-z]{2}[159BFJNRVZdhlptx]wZWRyYW([\\x2b\\x2f-9A-Za-z][\\x2b\\x2f-9A-Za-z]|[\\x2b\\x2f-9A-Za-z])*[\\x2b\\x2f-9A-Za-z][2GWm]FtaW5p|[\\x2b\\x2f-9A-Za-z]{2}[159BFJNRVZdhlptx]wZWRyYW([\\x2b\\x2f-9A-Za-z][\\x2b\\x2f-9A-Za-z]|[\\x2b\\x2f-9A-Za-z])*[\\x2b\\x2f-9A-Za-z][159BFJNRVZdhlptx]hbWlua[Q-Za-f])",
-  "success": true
-}
-"response=200"
+import pytest
+import requests_mock
+import requests
 
-{
-  "data": "([\\x2b\\x2f-9A-Za-z][AQgw]BwAGUAZAByAGEAbQB[0-6h-z]ACA{2}dwBhAHMA[\\x2b\\x2f-9A-Za-z][AQgw]BoAGUAcgBl|[\\x2b\\x2f-9A-Za-z]{2}[048AEIMQUYcgkosw]AcABlAGQAcgBhAG0A[YZa-e][AQgw]AgAHcAYQBzA[A-P][048AEIMQUYcgkosw]AaABlAHIAZ[Q-Za-f]|AHA{2}ZQBkAHIAYQBtA[GH][048AEIMQUYcgkosw]AIAB3AGEAcw[A-D][\\x2b\\x2f-9A-Za-z]AGgAZQByAG[U-X])",
-  "success": true
-}
-"response=200"
+from inquestlabs import inquestlabs_exception
 
-{
-  "data": "([\\x2b\\x2f-9A-Za-z][3HXn]A{2}ZQBkAHIAYQBtA[GH][048AEIMQUYcgkosw]AIAB3AGEAcw[A-D][\\x2b\\x2f-9A-Za-z]AGgAZQByAGUA|cABlAGQAcgBhAG0A[YZa-e][AQgw]AgAHcAYQBzA[A-P][048AEIMQUYcgkosw]AaABlAHIAZQ[A-D]|[\\x2b\\x2f-9A-Za-z]{2}[159BFJNRVZdhlptx]wAGUAZAByAGEAbQB[0-6h-z]ACA{2}dwBhAHMA[\\x2b\\x2f-9A-Za-z][AQgw]BoAGUAcgBlA[A-P])",
-  "success": true
-}
-"response=200"`````````````
-"""
+@pytest.fixture
+def mock_body():
+    return """([\x2b\x2f-9A-Za-z][3HXn]BlZHJhb([\x2b\x2f-9A-Za-z][\x2b\x2f-9A-Za-z]|[\x2b\x2f-9A-Za-z])*[\x2b\x2f-9A-Za-z][159BFJNRVZdhlptx]hbWlua[Q-Za-f]|[\x2b\x2f-9A-Za-z]{2}[159BFJNRVZdhlptx]wZWRyYW[0-3][\x2b\x2f-9A-Za-z]YW1pbm[k-n]|[\x2b\x2f-9A-Za-z][3HXn]BlZHJhb([\x2b\x2f-9A-Za-z][\x2b\x2f-9A-Za-z]|[\x2b\x2f-9A-Za-z])*[\x2b\x2f-9A-Za-z]{2}[2GWm]FtaW5p|cGVkcmFt([\x2b\x2f-9A-Za-z][\x2b\x2f-9A-Za-z]|[\x2b\x2f-9A-Za-z])*[\x2b\x2f-9A-Za-z][159BFJNRVZdhlptx]hbWlua[Q-Za-f]|[\x2b\x2f-9A-Za-z][3HXn]BlZHJhb([\x2b\x2f-9A-Za-z][\x2b\x2f-9A-Za-z]|[\x2b\x2f-9A-Za-z])*[\x2b\x2f-9A-Za-z]{2}YW1pbm[k-n]|cGVkcmFtYW1pbm[k-n]|[\x2b\x2f-9A-Za-z]{2}[159BFJNRVZdhlptx]wZWRyYW1hbWlua[Q-Za-f]|cGVkcmFt([\x2b\x2f-9A-Za-z][\x2b\x2f-9A-Za-z]|[\x2b\x2f-9A-Za-z])*[\x2b\x2f-9A-Za-z][2GWm]FtaW5p|[\x2b\x2f-9A-Za-z][3HXn]BlZHJhb([\x2b\x2f-9A-Za-z][\x2b\x2f-9A-Za-z]|[\x2b\x2f-9A-Za-z])*[\x2b\x2f-9A-Za-z][2GWm]FtaW5p|[\x2b\x2f-9A-Za-z]{2}[159BFJNRVZdhlptx]wZWRyYW([\x2b\x2f-9A-Za-z][\x2b\x2f-9A-Za-z]|[\x2b\x2f-9A-Za-z])*[\x2b\x2f-9A-Za-z]{2}[2GWm]FtaW5p|[\x2b\x2f-9A-Za-z][3HXn]BlZHJhbWFtaW5p|cGVkcmFt([\x2b\x2f-9A-Za-z][\x2b\x2f-9A-Za-z]|[\x2b\x2f-9A-Za-z])*[\x2b\x2f-9A-Za-z]{2}[2GWm]FtaW5p|cGVkcmFt([\x2b\x2f-9A-Za-z][\x2b\x2f-9A-Za-z]|[\x2b\x2f-9A-Za-z])*[\x2b\x2f-9A-Za-z]{2}YW1pbm[k-n]|cGVkcmFt[\x2b\x2f-9A-Za-z][2GWm]FtaW5p|[\x2b\x2f-9A-Za-z]{2}[159BFJNRVZdhlptx]wZWRyYW([\x2b\x2f-9A-Za-z][\x2b\x2f-9A-Za-z]|[\x2b\x2f-9A-Za-z])*[\x2b\x2f-9A-Za-z]{2}YW1pbm[k-n]|[\x2b\x2f-9A-Za-z][3HXn]BlZHJhb[Q-Za-f][159BFJNRVZdhlptx]hbWlua[Q-Za-f]|[\x2b\x2f-9A-Za-z]{2}[159BFJNRVZdhlptx]wZWRyYW([\x2b\x2f-9A-Za-z][\x2b\x2f-9A-Za-z]|[\x2b\x2f-9A-Za-z])*[\x2b\x2f-9A-Za-z][2GWm]FtaW5p|[\x2b\x2f-9A-Za-z]{2}[159BFJNRVZdhlptx]wZWRyYW([\x2b\x2f-9A-Za-z][\x2b\x2f-9A-Za-z]|[\x2b\x2f-9A-Za-z])*[\x2b\x2f-9A-Za-z][159BFJNRVZdhlptx]hbWlua[Q-Za-f])"""
+
+@pytest.fixture
+def mock_regex():
+  return "pedram.*amini"
+
+
+def test_valid_b64re(labs,mocker,mock_body, mock_regex):
+    mocker.patch('inquestlabs.inquestlabs_api.API', return_value=mock_body)
+    results = labs.yara_b64re(mock_regex)
+    assert mock_body in results
+
+def test_valid_b64re_big_endian(labs,mock_regex ,mocker,mock_body):
+    mocker.patch('inquestlabs.inquestlabs_api.API', return_value=mock_body)
+    results = labs.yara_b64re(mock_regex,endian="BIG")
+    assert mock_body in results
+
+def test_valid_b64re_little_endian(labs,mocker,mock_body,mock_regex):
+    mocker.patch('inquestlabs.inquestlabs_api.API', return_value=mock_body)
+    results = labs.yara_b64re(mock_regex,endian="LITTLE")
+    assert mock_body in results
+
+def test_valid_b64re_big_endian_with_key(labs_with_key,mock_regex,mocker,mock_body):
+    mocker.patch('inquestlabs.inquestlabs_api.API', return_value=mock_body)
+    results = labs_with_key.yara_b64re(mock_regex,endian="BIG")
+    assert mock_body in results
+
+def test_valid_b64re_little_endian_with_key(labs_with_key,mock_regex,mocker,mock_body):
+    mocker.patch('inquestlabs.inquestlabs_api.API', return_value=mock_body)
+    results = labs_with_key.yara_b64re(mock_regex,endian="LITTLE")
+    assert mock_body in results
+
+def test_valid_b64re_with_key(labs_with_key,mocker,mock_body,mock_regex):
+    mocker.patch('inquestlabs.inquestlabs_api.API', return_value=mock_body)
+    results = labs_with_key.yara_b64re(mock_regex)
+    assert mock_body in results
+
+def test_invalid_endian(labs,mocker,mock_regex):
+    with pytest.raises(inquestlabs_exception) as excinfo:
+        labs.yara_b64re(mock_regex,endian="BAD_ENDIAN")
+        
+    assert "invalid endianess" in str(excinfo.value)
+
+def test_invalid_endian_with_key(labs_with_key,mocker,mock_regex):
+    with pytest.raises(inquestlabs_exception) as excinfo:
+        labs_with_key.yara_b64re(mock_regex,endian="BAD_ENDIAN")
+        
+    assert "invalid endianess" in str(excinfo.value)
