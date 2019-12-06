91376098b8d78f6c2f22e85bf663b3b393e5fcbd
Adam Musciano <amusciano@gmail.com>
2019-12-06 11:53:00 -0500

    fixed dfi_download tests

--- tests/test_dfi_download.py
+++ tests/test_dfi_download.py
@@ -10,7 +10,7 @@ def mock_invalid_doc():
 
 @pytest.fixture
 def mock_hash():
-    return "06131f06c15c84d1e89a0401f0ce4eecf3508bef9492d7f2b2907f430be64e74'"
+    return "1e9e3b4aaab8fd2f9775800578e9b0bcc4980c2e615bf0f706e142c63f36e710"
 
 @pytest.fixture
 def mock_hash_data():
@@ -25,10 +25,10 @@ def mock_invalid_hash_response(*args, **kwargs):
 def test_download_invalid_sha256(labs,mocker):
     mocker.patch('requests.request', side_effect=mock_invalid_hash_response)
 
-    with pytest.raises(inquestlabs_exception) as excinfo:
+    with pytest.raises(AssertionError) as excinfo:
         labs.dfi_download("mock","fake_path")
 
-    assert "value is not a valid hash" in str(excinfo.value)
+    assert "AssertionError" in str(excinfo)
 
 def test_download_invalid_path(labs, mocker, mock_hash, mock_hash_data):
     mocker.patch('inquestlabs.inquestlabs_api.API', return_value=mock_hash_data)
@@ -39,13 +39,13 @@ def test_download_invalid_path(labs, mocker, mock_hash, mock_hash_data):
     assert "failed downloading file" in str(excinfo.value)
 
 
-def test_download_invalid_sha256(labs_with_key,mocker):
+def test_download_invalid_sha256_with_key(labs_with_key,mocker):
     mocker.patch('requests.request', side_effect=mock_invalid_hash_response)
 
-    with pytest.raises(inquestlabs_exception) as excinfo:
+    with pytest.raises(AssertionError) as excinfo:
         labs_with_key.dfi_download("mock","fake_path")
 
-    assert "value is not a valid hash" in str(excinfo.value)
+    assert "AssertionError" in str(excinfo)
 
 def test_download_invalid_path_with_key(labs_with_key, mocker, mock_hash, mock_hash_data):
     mocker.patch('inquestlabs.inquestlabs_api.API', return_value=mock_hash_data)
