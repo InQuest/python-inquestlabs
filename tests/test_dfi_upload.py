ba51c20427c2c7ad2d456c1ac35072a140ddcef7
Chris Morrow <cmmorrow@gmail.com>
2020-10-12 19:25:08 -0500

    Modified the upload file type check to use slice notation instead of read size. Modified the dfi upload tests to compare with a bytes object instead of a string.

--- tests/test_dfi_upload.py
+++ tests/test_dfi_upload.py
@@ -19,25 +19,25 @@ def mock_valid_response():
 
 
 def test_invalid_upload_type(labs, mocker, mock_invalid_doc):
+    mock_file = mocker.mock_open(read_data=mock_invalid_doc)
     mocker.patch("os.path.exists", return_value=True)
     mocker.patch("os.path.isfile", return_value=True)
-    mocker.patch('builtins.open', mocker.mock_open(
-        read_data='invalid'), create=True)
-    with pytest.raises(inquestlabs_exception) as excinfo:
+    mocker.patch('builtins.open', mock_file)
+    mocker.patch('inquestlabs.inquestlabs_api.API',
+                 return_value=mock_valid_response)
+    with pytest.raises(inquestlabs_exception, match=r'unsupported file type for upload'):
         labs.dfi_upload("mock")
-
-    assert "unsupported file type" in str(excinfo.value)
 
 
 def test_valid_upload_type(labs, mocker, mock_valid_doc, mock_valid_response):
+    mock_file = mocker.mock_open(read_data=b'PK')
     mocker.patch("os.path.exists", return_value=True)
     mocker.patch("os.path.isfile", return_value=True)
-    mocker.patch('builtins.open', mocker.mock_open(
-        read_data='PK'), create=True)
+    mocker.patch('builtins.open', mock_file)
     mocker.patch('inquestlabs.inquestlabs_api.API',
                  return_value=mock_valid_response)
     response = labs.dfi_upload("mock")
-    assert response["success"] == True
+    assert response["success"]
 
 
 def test_nonexistant_path(labs, mocker):
@@ -57,26 +57,26 @@ def test_path_is_not_a_file(labs, mocker):
 
 
 def test_invalid_upload_type_with_key(labs_with_key, mocker, mock_invalid_doc):
+    mock_file = mocker.mock_open(read_data=mock_invalid_doc)
     mocker.patch("os.path.exists", return_value=True)
     mocker.patch("os.path.isfile", return_value=True)
-    mocker.patch('builtins.open', mocker.mock_open(
-        read_data=mock_invalid_doc), create=True)
-    with pytest.raises(inquestlabs_exception) as excinfo:
+    mocker.patch('builtins.open', mock_file)
+    mocker.patch('inquestlabs.inquestlabs_api.API',
+                 return_value=mock_valid_response)
+    with pytest.raises(inquestlabs_exception, match=r'unsupported file type for upload'):
         labs_with_key.dfi_upload("mock")
-
-    assert "unsupported file type" in str(excinfo.value)
 
 
 def test_valid_upload_type_with_key(labs_with_key, mocker, mock_valid_doc, mock_valid_response):
+    mock_file = mocker.mock_open(read_data=b'PK')
     mocker.patch("os.path.exists", return_value=True)
     mocker.patch("os.path.isfile", return_value=True)
-    mocker.patch('builtins.open', mocker.mock_open(
-        read_data='PK'), create=True)
+    mocker.patch('builtins.open', mock_file, create=True)
     mocker.patch('inquestlabs.inquestlabs_api.API',
                  return_value=mock_valid_response)
     response = labs_with_key.dfi_upload("mock")
 
-    assert response["success"] == True
+    assert response["success"]
 
 
 def test_nonexistant_path_with_key(labs_with_key, mocker):
